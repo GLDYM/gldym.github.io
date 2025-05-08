a045448970c23b7d48199fe4def30028cf44ed98
Polaris_Light <995905922@qq.com>
2025-05-08 11:23:51 +0800

    Site updated: 2025-05-08 11:23:50

--- js/backgroundize.js
+++ js/backgroundize.js
@@ -1,6 +1,6 @@
 document
   .querySelector('#web_bg')
-  .setAttribute('style', `background-image: ${document.querySelector('.banner').style.background.split(' ')[0]};position: fixed;width: 100%;height: 100%;z-index: -1;background-size: cover;`);
+  .setAttribute('style', `background-blend-mode: multiply;background-color: rgba(0,0,0,0.4);background-image: ${document.querySelector('.banner').style.background.split(' ')[0]};position: fixed;width: 100%;height: 100%;z-index: -1;background-size: cover;`);
 document
   .querySelector("#banner")
   .setAttribute('style', 'background-image: url()')
--- js/time-insert.js
+++ js/time-insert.js
@@ -3,16 +3,12 @@ var newDiv = document.createElement("div");
 newDiv.innerHTML = `
       <span id="time"></span>
       <script src="/js/time.js"></script>
-`;
+      `;
 
 document.addEventListener("DOMContentLoaded", function() {
-  var div1 = document.getElementsByClassName("statistics")[0];
-  var div2 = document.getElementsByClassName("beian")[0];
+  var target = document.getElementsByClassName("footer-content")[0];
 
-  console.log(div1); 
-  console.log(div2);  
- 
-  if (div1 && div2) {
-    div1.parentNode.insertBefore(newDiv, div2);
+  if (target) {
+    target.parentNode.insertBefore(newDiv, target);
   }
 });
