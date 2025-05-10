78558c9a0d477ae18c6d4b27683fdb0214eb4d7b
Polaris_Light <995905922@qq.com>
2025-05-10 19:01:42 +0800

    Site updated: 2025-05-10 19:01:39

--- js/backgroundize.js
+++ js/backgroundize.js
@@ -1,6 +1,6 @@
 document
   .querySelector('#web_bg')
-  .setAttribute('style', `background-blend-mode: multiply;background-color: rgba(0,0,0,0.4);background-image: ${document.querySelector('.banner').style.background.split(' ')[0]};background-size: cover;background-position: center;position: fixed;width: 100vw;height: 100vh;z-index: -1;transform: translate(0px, 0px);`);
+  .setAttribute('style', `background-blend-mode: multiply;background-color: rgba(0,0,0,0.4);background-image: url("/img/bg.webp");background-size: cover;background-position: center;position: fixed;width: 100vw;height: 100vh;z-index: -1;transform: translate(0px, 0px);`);
 document
   .querySelector('#web_bg')
   .setAttribute('parallax', 'true')
